da6da9d2fbdca1e1190d8dc3f5fdb8ce3f560c3c
Samuel Evangelista <samuel06eva@gmail.com>
2026-02-21 18:45:07 -0300

    Inicio do projeto Spring

---
+++ lumina/src/main/java/br/com/samu/lumina/model/DadosSerie.java
@@ -0,0 +1,12 @@
+package br.com.samu.lumina.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao) {
+
+}
