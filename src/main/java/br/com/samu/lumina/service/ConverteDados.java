531900f61945f6025c0ee48efe14e6500db1f6d8
Samuel Evangelista <samuel06eva@gmail.com>
2026-02-21 19:30:03 -0300

    Arrumando a estrutura do projeto: movendo para a raiz.

---
+++ src/main/java/br/com/samu/lumina/service/ConverteDados.java
@@ -0,0 +1,20 @@
+package br.com.samu.lumina.service;
+
+import br.com.samu.lumina.model.DadosSerie;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados{
+     private final ObjectMapper mapper = new ObjectMapper();
+
+    public ConverteDados() {
+    }
+
+    public <T> T obterDados(String json, Class<T> classe) {
+        try {
+            return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/br/com/samu/lumina/LuminaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.samu.lumina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuminaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
